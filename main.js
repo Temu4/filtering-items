f4e1b89e02cac7fa13bbf3462aa30aa954345046
Temu4 <artem.vorobjov@gmail.com>
2022-02-11 13:44:25 +0200

    update filtering, add inputs dynamicly

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 import ITEMS from './items.js';
 
 const createItemHTML = (item) => {
-  return `<div class="item item--${item.os}"><b>${item.name}</b> - os: <i>${item.os}</i></div>`;
+  return `<div class="item item--${item.os}"><b>${item.name}</b>
+          <br>
+          os: <i>${item.os}</i>
+          <br>
+          colors: <i>${item.color.join(', ')}</i>
+          <br>
+          price: $${item.price}
+  </div>`;
 };
 
 const generatePageContent = (items) => {
@@ -15,43 +22,169 @@ const generatePageContent = (items) => {
   content.innerHTML = data || 'No items based on your filters';
 };
 
-const inputName = document.getElementById('name');
-const inputIOS = document.getElementById('IOS');
-const inputMacOS = document.getElementById('macOS');
+const createInputField = ({type, name, value, data, label, labelPosition = 'left'}) => `
+  ${label ? '<label>' : ''}
+    ${labelPosition === 'left' ? label : ''}
+      <input 
+        type="${type}"
+        name="${name}"
+        ${value ? `value="${value}"` : ''}
+        ${data ? `data-type="${data}"` : ''}>
+      ${labelPosition === 'right' ? label : ''}
+    ${label ? '</label>' : ''}
+`;
 
-const filterItems = () => {
-  const valueName = inputName.value.toLowerCase();
-  const valueIOS = inputIOS.checked ? inputIOS.name : '';
-  const valueMacOS = inputMacOS.checked ? inputMacOS.name : '';
+const generateFiltersContent = (items) => {
+  const filtersWrapper = document.getElementById('filters');
 
-  const filteredItems = ITEMS.filter((item) => {
-    // true | false
-
-    if (valueName && !item.name.toLowerCase().includes(valueName)) {
-      return false;
+  const filtersValues = items.reduce((acc, item) => {
+    if (item.os) {
+      if (acc.os) {
+        !acc.os.includes(item.os) && acc.os.push(item.os);
+      } else {
+        acc.os = [item.os];
+      }
     }
 
-    if (valueIOS && item.os !== valueIOS) {
-      return false;
+    if (item.color) {
+      if (acc.color) {
+        item.color.forEach((color) => {
+          !acc.color.includes(color) && acc.color.push(color);
+        });
+      } else {
+        acc.color = [...item.color];
+      }
     }
 
-    return true;
-  });
+    return acc;
+  }, {});
 
-  return filteredItems;
+  console.log('filtersValues: ', filtersValues);
+
+  let inputs = '';
+
+  // add name field
+  inputs += createInputField({name: 'name', type: 'text', label: 'Name'});
+  inputs += '<hr>';
+
+  // add os fields
+  if (filtersValues.os && filtersValues.os.length) {
+    inputs += '<p>OS:</p><br>';
+    inputs += filtersValues.os.reduce((acc, os) => {
+      acc += createInputField({
+        type: 'checkbox',
+        name: 'os',
+        value: os,
+        label: os,
+        labelPosition: 'right'
+      });
+
+      return acc;
+    }, '');
+    inputs += '<hr>';
+  }
+
+  // add color fields
+  if (filtersValues.color && filtersValues.color.length) {
+    inputs += '<p>Colors:</p><br>';
+    inputs += filtersValues.color.reduce((acc, color) => {
+      acc += createInputField({
+        type: 'checkbox',
+        name: 'color',
+        value: color,
+        label: color,
+        labelPosition: 'right'
+      });
+
+      return acc;
+    }, '');
+    inputs += '<hr>';
+  }
+
+  // add price field
+  inputs += createInputField({name: 'price', type: 'number', label: 'Min price', data: 'min'});
+  inputs += createInputField({name: 'price', type: 'number', label: 'Max price', data: 'max'});
+  inputs += '<hr>';
+
+  filtersWrapper.innerHTML = inputs;
 };
 
-inputName.addEventListener('input', () => {
-  generatePageContent(filterItems());
-});
-
-inputIOS.addEventListener('change', () => {
-  generatePageContent(filterItems());
-});
-
-inputMacOS.addEventListener('change', () => {
-  generatePageContent(filterItems());
-});
-
 // init
-document.addEventListener('DOMContentLoaded', () => generatePageContent(ITEMS));
+document.addEventListener('DOMContentLoaded', () => {
+  generateFiltersContent(ITEMS);
+  generatePageContent(ITEMS);
+
+  const inputs = document.querySelectorAll('input');
+
+  const filterItems = () => {
+    // const values = Array.from(inputs).reduce((acc, input) => {
+    // condition
+
+    //   return acc;
+    // }, {});
+
+    // const values = {
+    //   // name
+    //   // os
+    //   // price
+    // }
+
+    let valueName = '';
+    let valuesOS = [];
+    let valuesColor = [];
+    const valuePrice = {};
+
+    inputs.forEach((input) => {
+      if (input.name === 'name') {
+        valueName = input.value.toLowerCase();
+      }
+
+      if (input.name === 'os' && input.checked) valuesOS.push(input.value);
+
+      if (input.name === 'color' && input.checked) valuesColor.push(input.value);
+
+      if (input.name === 'price') {
+        input.value && (valuePrice[input.dataset.type] = +input.value);
+      }
+    });
+
+    const filteredItems = ITEMS.filter((item) => {
+      // true | false
+
+      if (valueName && !item.name.toLowerCase().includes(valueName)) {
+        return false;
+      }
+
+      if (valuesOS.length) {
+        if (!valuesOS.includes(item.os)) {
+          return false;
+        }
+      }
+
+      if (valuesColor.length) {
+        if (!valuesColor.some((value) => item.color.includes(value))) {
+          return false;
+        }
+      }
+
+      if (
+        (valuePrice.min && item.price < valuePrice.min) ||
+        (valuePrice.max && item.price > valuePrice.max)
+      ) {
+        return false;
+      }
+
+      return true;
+    });
+
+    return filteredItems;
+  };
+
+  inputs.forEach((input) => {
+    const eventType = input.type === 'checkbox' ? 'change' : 'input';
+
+    input.addEventListener(eventType, () => {
+      generatePageContent(filterItems());
+    });
+  });
+});
